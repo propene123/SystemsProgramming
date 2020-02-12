147b6cd5d7fb3a59196192cc61411f2a622504fd
propene123 <benjamin.russell@durham.ac.uk>
2020-02-12 16:22:23 +0000

    almost done

--- Prog/src/gameoflife.c
+++ Prog/src/gameoflife.c
@@ -3,7 +3,12 @@
 
 int main(int argc, char** argv) {
     struct universe u;
-    read_in_file(stdin, &u);
+    FILE* in = fopen("glider.txt", "r");
+    read_in_file(in, &u);
+    for(int i = 0;i<5;++i) {
+        evolve(&u, will_be_alive);
+    }
+    
     for(int i = 0;i<u.height;++i){
         for(int j = 0;j<u.width;++j){
             printf("%c", u.board[i][j]);
--- Prog/src/libgol/gol.c
+++ Prog/src/libgol/gol.c
@@ -25,11 +25,14 @@ int getLine(char* buffer, int maxCount, FILE* file){
     return count;
 }
 
-void freeBoard(char **board, int rows) {
+// probably uncessary to ever use this as it would only be used when calling exit anyway
+void freeBoard(char ***board, int rows) {
     for(int i = 0;i<rows;++i) {
-        free(board[i]);
+        free((*board)[i]);
+        (*board)[i] = NULL;
     }
     free(board);
+    *board = NULL;
 }
 
 
@@ -54,22 +57,21 @@ void read_in_file(FILE *infile, struct universe *u){
         u->board[rows-1] = malloc(columns*sizeof(char));
         if(u->board[rows-1] == NULL){
             fprintf(stderr, "Could not allocate memory for input file\n");
+            freeBoard(&u->board, 0);
             exit(EXIT_FAILURE);
         }
         memcpy(u->board[rows-1], buff, columns); // copy from buffer to main board
         // while there are still characters to read
         while((columnCount = getLine(buff, 513, infile)) != 0) {
             if(columnCount < 0 || columnCount != columns) {
-                freeBoard(u->board, rows);
-                u->board = NULL;
+                freeBoard(&u->board, rows);
                 fprintf(stderr, "Input file is invalid\n");
                 exit(EXIT_FAILURE);
             }
             ++rows;
             char **tmp = realloc(u->board, rows*sizeof(char*));
             if(tmp == NULL) {
-                freeBoard(u->board, rows);
-                u->board = NULL;
+                freeBoard(&u->board, rows);
                 fprintf(stderr, "Could not allocate memory for input file\n");
                 exit(EXIT_FAILURE);
             }
@@ -77,8 +79,7 @@ void read_in_file(FILE *infile, struct universe *u){
             tmp = NULL;
             u->board[rows-1] = malloc(columns*sizeof(char));
             if(u->board[rows-1] == NULL){
-                freeBoard(u->board, rows);
-                u->board = NULL;
+                freeBoard(&u->board, rows);
                 fprintf(stderr, "Could not allocate memory for input file\n");
                 exit(EXIT_FAILURE);
             }
@@ -97,22 +98,103 @@ void write_out_file(FILE *outfile, struct universe *u){
 
 
 int is_alive(struct universe *u, int column, int row){
-
+    if(row < 0 || row > (u->height - 1)) {
+        fprintf(stderr, "is_alive: trying to access a non exisitent cell");
+        // free the board we have, it will be freed on exit anyway but may as well do it now
+        freeBoard(&u->board, u->height);
+        exit(EXIT_FAILURE);
+    }
+    return (u->board[row][column] == '*');
 }
 
 
 int will_be_alive(struct universe *u, int column, int row){
-
+    int aliveCount = 0;
+    for (int i = row - 1 ; i < row + 2; ++i) {
+        for (int j = column - 1;j < column +2;++j) {
+            if(i < 0 || j < 0 || i > (u->height-1) || j > (u->width - 1)) {
+                continue;
+            }else if(is_alive(u, j, i)) {
+                ++aliveCount;
+            }
+        }
+    }
+    if(is_alive(u, column, row)) {
+        if(aliveCount == 3 || aliveCount == 4) {
+            return 1;
+        }
+    }else {
+        if(aliveCount == 3) {
+            return 1;
+        }
+    }
+    return 0;
 }
 
 
 int will_be_alive_torus(struct universe *u,  int column, int row){
-
+    int aliveCount = 0;
+    for (int i = row - 1 ; i < row + 2; ++i) {
+        for (int j = column - 1;j < column +2;++j) {
+            int r = i;
+            int c = j;
+            if(i == -1) {
+                r = u->height-1;
+            }else if(i == u->height) {
+                r = 0;
+            }
+            if(j == -1) {
+                c = u->width-1;
+            }else if(j == u->width) {
+                c = 0;
+            }
+            if(is_alive(u, c, r)) {
+                ++aliveCount;
+            }
+        }
+    }
+    if(is_alive(u, column, row)) {
+        if(aliveCount == 3 || aliveCount == 4) {
+            return 1;
+        }
+    }else {
+        if(aliveCount == 3) {
+            return 1;
+        }
+    }
+    return 0;
 }
 
 
 void evolve(struct universe *u, int (*rule)(struct universe *u, int column, int row)){
-
+    //create tmp buffer to store new state
+    char **tmp = malloc(u->height*sizeof(char*));
+    if(tmp == NULL) {
+        fprintf(stderr, "Memory error while evolving game state");
+        exit(EXIT_FAILURE);
+    }
+    for(int i = 0;i<u->height;++i) {
+        tmp[i] = malloc(u->width * sizeof(char));
+        if(tmp[i] == NULL) {
+            fprintf(stderr, "Memory error while evolving game state");
+            freeBoard(&tmp, i);
+            exit(EXIT_FAILURE);
+        }
+    }
+    for(int i = 0; i<u->height;++i) {
+        for(int j = 0;j<u->width;++j) {
+            if(rule(u, j,i)) {
+                tmp[i][j] = '*';
+            }else {
+                tmp[i][j] = '.';
+            }
+        }
+    }
+    //free old board
+    freeBoard(&u->board, u->height);
+    //assign universe board to tmp buffer
+    u->board = tmp;
+    tmp = NULL;
 }
 
 
